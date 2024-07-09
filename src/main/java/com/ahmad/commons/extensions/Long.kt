75f5f8e29640f37ba3fc2592ff6f454630ca77ce
aheadmujtaba <42637803+aheadmujtaba@users.noreply.github.com>
2024-07-09 17:42:29 +0500

    lots of things are done no time to explain

--- src/main/java/com/ahmad/commons/extensions/Long.kt
+++ src/main/java/com/ahmad/commons/extensions/Long.kt
@@ -1,21 +1,31 @@
 package com.ahmad.commons.extensions
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 fun Long.formatToTimeDuration(): String {
-   return runCatching {
+    return runCatching {
 
         val totalSeconds = this / 1000
         val hours = totalSeconds / 3600
         val minutes = (totalSeconds % 3600) / 60
         val seconds = totalSeconds % 60
 
-       if (hours > 0) {
+        if (hours > 0) {
             String.format("%d:%02d:%02d", hours, minutes, seconds)
         } else {
             String.format("%d:%02d", minutes, seconds)
         }
     }.onFailure {
         it.printStackTrace()
-   }.getOrElse {
-       "?:??"
-   }
+    }.getOrElse {
+        "?:??"
+    }
+}
+
+fun Long.toDate(): String {
+    return SimpleDateFormat("MM/dd/yyyy", Locale.getDefault()).run {
+        format(this)
+    }
+
 }
