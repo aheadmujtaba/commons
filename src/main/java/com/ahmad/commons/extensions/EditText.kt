b581486dac775be2c129c9ba833723f4b8fae06a
aheadmujtaba <42637803+aheadmujtaba@users.noreply.github.com>
2024-07-22 18:03:13 +0500

    i am lazy donot want to add git comments

--- src/main/java/com/ahmad/commons/extensions/EditText.kt
+++ src/main/java/com/ahmad/commons/extensions/EditText.kt
@@ -1,5 +1,7 @@
 package com.ahmad.commons.extensions
 
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import kotlin.math.max
 import kotlin.math.min
@@ -8,4 +10,10 @@ fun EditText.insertTextAtCursorLocation(text: String) {
     val start = this.selectionStart
     val end = this.selectionEnd
     this.text.replace(min(start, end), max(start, end), text, 0, text.length)
+}
+
+fun EditText.openKeyboard(){
+    this.requestFocus()
+    val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(this,InputMethodManager.SHOW_IMPLICIT)
 }
